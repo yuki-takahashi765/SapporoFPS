f6b2e427eae19396cc7c4a4dfa560e4c1c0a38f4
acro-takahashi <91651462+acro-takahashi@users.noreply.github.com>
2021-10-07 23:40:02 +0900

    add #4

--- Assets/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
+++ Assets/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
@@ -178,7 +178,8 @@ namespace UnityChan
 			}
 		}
 
-		void OnGUI ()
+		/*
+		 void OnGUI ()
 		{
 			GUI.Box (new Rect (Screen.width - 260, 10, 250, 150), "Interaction");
 			GUI.Label (new Rect (Screen.width - 245, 30, 250, 30), "Up/Down Arrow : Go Forwald/Go Back");
@@ -188,7 +189,7 @@ namespace UnityChan
 			GUI.Label (new Rect (Screen.width - 245, 110, 250, 30), "Left Control : Front Camera");
 			GUI.Label (new Rect (Screen.width - 245, 130, 250, 30), "Alt : LookAt Camera");
 		}
-
+		*/
 
 		// キャラクターのコライダーサイズのリセット関数
 		void resetCollider ()
