678af7a3d36152cb22c3e085bfaf4308fd0d6c98
acro-takahashi <91651462+acro-takahashi@users.noreply.github.com>
2021-10-14 19:11:42 +0900

    #10 仮fixd

--- Assets/00.Yuki/EnemyMove.cs
+++ Assets/00.Yuki/EnemyMove.cs
@@ -5,6 +5,43 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     public ParticleSystem effect;//インスペクター上にエフェクト設定が行える用
+    private float timeElapsed;
+    public float X=-0.05f;
+    public float Y=0f;
+    public float Z = 0;
+    public float X2= 0.05f;
+    public float Y2= 0.15f;
+    public float Z2=0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed <= 1)
+        {
+            transform.Translate(X, Y, Z);
+            return;
+        }
+        else if (timeElapsed <= 2)
+        {
+            return;
+        }
+        else if (timeElapsed <= 3)
+        {
+            transform.Translate(X2, Y2, Z2);
+            return;
+        }
+        else if (timeElapsed <= 4)
+        {
+            return;
+        }
+        timeElapsed = 0;
+
+    }
+
     void OnCollisionEnter(Collision obj)
     {
         if (obj.transform.name == "ShotCube(Clone)")
