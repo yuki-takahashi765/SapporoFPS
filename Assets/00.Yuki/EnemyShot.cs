5b088d21e35b31c2559f5ad91d26c99216b61dd9
acro-takahashi <91651462+acro-takahashi@users.noreply.github.com>
2021-10-15 23:28:00 +0900

    Enemy move add

---
+++ Assets/00.Yuki/EnemyShot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShot : MonoBehaviour
+{
+
+    public GameObject enemyShotPrefab;
+    
+    [Header("自動で射出するまでの時間")]
+    public float shotInterval=1.0f;
+    float timeCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間計測
+        timeCount += Time.deltaTime;
+        //指定の秒数以上になった場合
+        if (timeCount >= shotInterval)
+        {
+            //カウントをリセット
+            timeCount = 0;
+            //弾を生成
+            var pos = transform.position + transform.forward * 2 + transform.up * 1;
+            Instantiate(enemyShotPrefab, pos, transform.rotation);
+           // Instantiate(enemyShotPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
--- Assets/00.Yuki/ShotMove.cs
+++ Assets/00.Yuki/ShotMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShotMove : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
