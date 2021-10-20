bee1d35c435b66b9ddabeaffe8e4b54efd934bcf
acro-takahashi <91651462+acro-takahashi@users.noreply.github.com>
2021-10-20 20:49:51 +0900

    死亡判定追加

--- Assets/00.Yuki/script/PlayerHit.cs
+++ Assets/00.Yuki/script/PlayerHit.cs
@@ -6,15 +6,26 @@ public class PlayerHit : MonoBehaviour
 {
     [Header("再生するパーティクルの設定")]
     public ParticleSystem effect;
-
+    public int HP = 2;
     void OnCollisionEnter(Collision obj)
     {
         if (obj.transform.name == "EnemyShot(Clone)")
         //　「""」の中はどのオブジェクトと衝突するか
         {
             effect.Play();//エフェクトに設定されていたものを再生
-            Debug.Log("hit");//デバッグ用。当たったときにコンソールにhitが出ていたらここまで通ってる
+            //Debug.Log("hit");//デバッグ用。当たったときにコンソールにhitが出ていたらここまで通ってる
             Destroy(obj.gameObject);
+
+            if (HP > 0) 
+            {
+                HP--;
+                Debug.Log(HP);
+                if (HP == 0)
+                {
+                    Debug.Log("Gameover");
+                }
+
+            }
         }
     }
 }
