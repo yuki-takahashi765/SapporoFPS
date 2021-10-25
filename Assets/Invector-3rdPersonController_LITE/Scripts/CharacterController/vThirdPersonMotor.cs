62e321f58cc0850396dce4e3affd7ee8ce3c795e
acro-takahashi <91651462+acro-takahashi@users.noreply.github.com>
2021-10-25 20:42:17 +0900

    add

--- Assets/Invector-3rdPersonController_LITE/Scripts/CharacterController/vThirdPersonMotor.cs
+++ Assets/Invector-3rdPersonController_LITE/Scripts/CharacterController/vThirdPersonMotor.cs
@@ -384,7 +384,7 @@ namespace Invector.vCharacterController
             [Range(1f, 20f)]
             public float movementSmooth = 6f;
             [Range(0f, 1f)]
-            public float animationSmooth = 0f;
+            public float animationSmooth = 0.2f;
             [Tooltip("Rotation speed of the character")]
             public float rotationSpeed = 16f;
             [Tooltip("Character will limit the movement to walk instead of running")]
@@ -394,7 +394,7 @@ namespace Invector.vCharacterController
             [Tooltip("Speed to Walk using rigidbody or extra speed if you're using RootMotion")]
             public float walkSpeed = 2f;
             [Tooltip("Speed to Run using rigidbody or extra speed if you're using RootMotion")]
-            public float runningSpeed = 0f;
+            public float runningSpeed = 4f;
             [Tooltip("Speed to Sprint using rigidbody or extra speed if you're using RootMotion")]
             public float sprintSpeed = 6f;
         }
