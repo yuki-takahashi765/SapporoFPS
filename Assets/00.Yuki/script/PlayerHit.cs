3c1a213e1415c36eb360235021eec7dc9f11137b
acro-takahashi <91651462+acro-takahashi@users.noreply.github.com>
2021-10-26 21:45:39 +0900

    #21 fix

--- Assets/00.Yuki/script/PlayerHit.cs
+++ Assets/00.Yuki/script/PlayerHit.cs
@@ -12,10 +12,13 @@ public class PlayerHit : MonoBehaviour
     [Header("再生するパーティクルの設定")]
     public ParticleSystem effect;
     public int HP = 2;
-    [Header("HP0のときに削除する自キャラを追加")]
-    public GameObject TestUnityChan;
+    [Header("HP0のときに削除する自キャラとカメラを追加")]
+    public GameObject playerPrefab;
+    public GameObject deleteCamera;
 
-    public float runningSpeed { get; private set; }
+    [Header("HP0のときに追加するカメラ")]
+    public GameObject addCamera;
+
 
     void OnCollisionEnter(Collision obj)
     {
@@ -23,31 +26,31 @@ public class PlayerHit : MonoBehaviour
         //　「""」の中はどのオブジェクトと衝突するか
         {
             effect.Play();//エフェクトに設定されていたものを再生
-            //Debug.Log("hit");//デバッグ用。当たったときにコンソールにhitが出ていたらここまで通ってる
             Destroy(obj.gameObject);
 
-            if (HP > 0) 
+            if (HP > 0)
             {
                 HP--;
-                Debug.Log(HP);
+                //Debug.Log(HP);
                 if (HP == 0)
                 {
+
+                    hitPointText.text = "       GameOver";
                     Debug.Log("操作OFF後");
-                    Destroy(this.GetComponent<vThirdPersonInput>());
-                    List<KeyCode> disabledKeys = new List<KeyCode>();
-                    Destroy(this.GetComponent<vThirdPersonMotor>());
-                    Destroy(TestUnityChan);
+                    Destroy(playerPrefab);
+                    Destroy(deleteCamera);
+
+                    var pos = transform.position + transform.forward + transform.up;
+                    var del = Instantiate(addCamera, pos, transform.rotation);
+                }
+                else
+                {
+                    string str = hitPointText.text;
+                    string str1 = str.Substring(0, str.IndexOf(":") + 1);
+                    string str2 = HP.ToString();
+                    hitPointText.text = str1 + str2;
                 }
             }
-
-            string str = hitPointText.text;
-            string str1 = str.Substring(0, str.IndexOf(":") + 1);
-            Debug.Log(str1);
-            string str2 = str.Substring(str.IndexOf(":") + 1);
-            Debug.Log(str2);
-            str2 =HP.ToString();
-            hitPointText.text = str1 + str2;
-
         }
     }
 
